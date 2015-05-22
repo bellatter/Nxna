6b7a9c76a774e2e3cb6b6e70e7027c6fef37fc6d
bellatter <bellatter@users.noreply.github.com>
2015-05-22 21:28:46 +0000

    Changed char*[] to char**

--- src/Platform/Windows/WindowsMain.cpp
+++ src/Platform/Windows/WindowsMain.cpp
@@ -1,16 +1,18 @@
 #include "../PlatformDefs.h"
+#include <cstdlib>
 
 #if defined NXNA_PLATFORM_WIN32 && !defined NXNA_PLATFORM_WIN32_SDL 
 
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
-extern int NxnaMain(int argc, const char* argv[]);
+extern int NxnaMain(int argc, const char** argv);
+
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     PSTR lpCmdLine, INT nCmdShow)
 {
-    return NxnaMain(0, nullptr);
+    return NxnaMain(__argc, (const char**)__argv);
 }
 
 #endif
