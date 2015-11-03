c94ed2c6052a21739c485abb4f9bd99b4b88dc92
bellatter <bellatter@68518e73-86c5-4bd8-91b1-978d42ada68b>
2015-11-03 19:30:22 +0000

    Added stopwatch code for apple

--- trunk/src/Utils/StopWatch.cpp
+++ trunk/src/Utils/StopWatch.cpp
@@ -1,21 +1,31 @@
 #include "StopWatch.h"
 
+
 #ifdef _WIN32
 #include <Windows.h>
+#elif defined NXNA_PLATFORM_APPLE
+#include <mach/mach.h>
+#include <mach/mach_time.h>
 #endif
 
 namespace Nxna
 {
 namespace Utils
 {
+#ifdef _WIN32
 	uint64_t StopWatch::m_frequency = 0;
-
+#elif defined NXNA_PLATFORM_APPLE
+	static mach_timebase_info_data_t m_info;
+#endif
+	
 	uint64_t StopWatch::GetCurrentTicks()
 	{
 #ifdef _WIN32
 		LARGE_INTEGER ticks;
 		QueryPerformanceCounter(&ticks);
 		return (uint64_t)ticks.QuadPart;
+#elif defined NXNA_PLATFORM_APPLE
+		return mach_absolute_time();
 #endif
 	}
 
@@ -24,14 +34,18 @@ namespace Utils
 		m_running = false;
 		m_timeElapsed = 0;
 
+#ifdef _WIN32
 		if (m_frequency == 0)
 		{
-#ifdef _WIN32
 			LARGE_INTEGER frequency;
 			QueryPerformanceFrequency(&frequency);
 			m_frequency = frequency.QuadPart;
-#endif
 		}
+#elif defined NXNA_PLATFORM_APPLE
+		if (m_info.denom == 0 ) {
+			mach_timebase_info(&m_info);
+		}
+#endif
 	}
 
 	void StopWatch::Start()
@@ -68,12 +82,20 @@ namespace Utils
 
 	uint64_t StopWatch::GetElapsedMilliseconds()
 	{
+#ifdef _WIN32
 		return GetElapsedTicks() * 1000 / m_frequency;
+#elif defined NXNA_PLATFORM_APPLE
+		return GetElapsedTicks() * m_info.numer / m_info.denom / 1000000;
+#endif
 	}
 
 	unsigned int StopWatch::GetElapsedMilliseconds32()
 	{
+#ifdef _WIN32
 		return (unsigned int)(GetElapsedTicks() * 1000 / m_frequency);
+#elif defined NXNA_PLATFORM_APPLE
+		return (unsigned int)(GetElapsedTicks() * m_info.numer / m_info.denom / 1000000);
+#endif
 	}
 }
 }
