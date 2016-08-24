e2fc557d833db932d29ca8928eaf71f5207bd54a
bellatter <bellatter@68518e73-86c5-4bd8-91b1-978d42ada68b>
2016-08-24 21:54:39 +0000

    Some fixes for non-Windows platforms

--- branches/Nxna2/src/Color.h
+++ branches/Nxna2/src/Color.h
@@ -28,7 +28,7 @@ namespace Nxna
 
 	typedef unsigned int PackedColor;
 
-#define NXNA_GET_PACKED_COLOR(c) (PackedColor)((unsigned int)c.A << 24 | (unsigned int)c.B << 16 | (unsigned int)c.G << 8 | c.R)
+#define NXNA_GET_PACKED_COLOR(c) (Nxna::PackedColor)((unsigned int)c.A << 24 | (unsigned int)c.B << 16 | (unsigned int)c.G << 8 | c.R)
 #define NXNA_GET_PACKED_COLOR_RGB_BYTES(r,g,b) (Nxna::PackedColor)((unsigned int)255 << 24 | (unsigned int)(b & 0xff) << 16 | (unsigned int)(g & 0xff) << 8 | (r & 0xff))
 #define NXNA_GET_UNPACKED_COLOR(c, r, g, b, a) { r == c & 0xff; g = (c >> 8) & 0xff; b = (c >> 16) & 0xff; a = (c >> 24) & 0xff; }
 }
