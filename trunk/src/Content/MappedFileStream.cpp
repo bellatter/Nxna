8208c77c4cd3aae472e79d81662023b13d944a1f
bellatter <bellatter@68518e73-86c5-4bd8-91b1-978d42ada68b>
2017-02-03 18:53:27 +0000

    now it's possible to try to open a new MappedfileStream without allocating memory unnecessarily

--- trunk/src/Content/MappedFileStream.cpp
+++ trunk/src/Content/MappedFileStream.cpp
@@ -37,7 +37,7 @@ namespace Content
 		m_fd = -1;
 #endif
 
-		Load(path);
+		Load(path, this);
 	}
 
 	MappedFileStream::~MappedFileStream()
@@ -55,73 +55,79 @@ namespace Content
 		}
 	}
 
-	void MappedFileStream::Load(const char* path)
+	bool MappedFileStream::Load(const char* path, MappedFileStream* result)
 	{
-		if (IsOpen() == false)
-		{
+		if (path == nullptr || result == nullptr)
+			return false;
+
+		if (result->IsOpen())
+			return false;
+
 #ifdef _WIN32
-			m_fp = nullptr;
+		result->m_fp = nullptr;
 #else
-			m_fd = -1;
+		result->m_fd = -1;
 #endif
-			m_memory = nullptr;
-			m_mapping = nullptr;
-			m_length = 0;
+		result->m_memory = nullptr;
+		result->m_mapping = nullptr;
+		result->m_length = 0;
 
 #if defined NXNA_PLATFORM_WIN32
-			m_fp = CreateFile(path, GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_READONLY, nullptr);
-			if (m_fp == INVALID_HANDLE_VALUE)
-			{
-				m_fp = nullptr;
-				return;
-			}
-
-			m_mapping = CreateFileMapping(m_fp, nullptr, PAGE_READONLY, 0, 0, nullptr);
-			if (m_mapping == nullptr)
-			{
-				CloseHandle(m_fp);
-				m_fp = nullptr;
-				return;
-			}
-
-			m_memory = (byte*)MapViewOfFile(m_mapping, FILE_MAP_READ, 0, 0, 0);
-			if (m_memory == nullptr)
-			{
-				CloseHandle(m_mapping);
-				CloseHandle(m_fp);
-				m_mapping = nullptr;
-				m_fp = nullptr;
-				return;
-			}
-
-			m_length = GetFileSize(m_fp, nullptr);
-#else
-			m_fd = open(path, O_RDONLY);
-			if (m_fd == -1)
-				return;
-			
-			// get the file size
-			struct stat statInfo;
-			if (fstat(m_fd, &statInfo) < 0)
-			{
-				close(m_fd);
-				m_fd = -1;
-				return;
-			}
-			
-			m_memory = (byte*)mmap(nullptr, statInfo.st_size, PROT_READ, MAP_PRIVATE, m_fd, 0);
-			if (m_memory == MAP_FAILED)
-			{
-				close(m_fd);
-				m_fd = -1;
-				m_memory = nullptr;
-				return;
-			}
-			
-			m_length = statInfo.st_size;
-#endif
-			m_position = 0;
+		result->m_fp = CreateFile(path, GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_READONLY, nullptr);
+		if (result->m_fp == INVALID_HANDLE_VALUE)
+		{
+			result->m_fp = nullptr;
+			return false;
 		}
+
+		result->m_mapping = CreateFileMapping(result->m_fp, nullptr, PAGE_READONLY, 0, 0, nullptr);
+		if (result->m_mapping == nullptr)
+		{
+			CloseHandle(result->m_fp);
+			result->m_fp = nullptr;
+			return false;
+		}
+
+		result->m_memory = (byte*)MapViewOfFile(result->m_mapping, FILE_MAP_READ, 0, 0, 0);
+		if (result->m_memory == nullptr)
+		{
+			CloseHandle(result->m_mapping);
+			CloseHandle(result->m_fp);
+			result->m_mapping = nullptr;
+			result->m_fp = nullptr;
+			return false;
+		}
+
+		result->m_length = GetFileSize(result->m_fp, nullptr);
+#else
+		result->m_fd = open(path, O_RDONLY);
+		if (result->m_fd == -1)
+			return false;
+			
+		// get the file size
+		struct stat statInfo;
+		if (fstat(result->m_fd, &statInfo) < 0)
+		{
+			close(result->m_fd);
+			result->m_fd = -1;
+			return false;
+		}
+			
+		result->m_memory = (byte*)mmap(nullptr, statInfo.st_size, PROT_READ, MAP_PRIVATE, result->m_fd, 0);
+		if (result->m_memory == MAP_FAILED)
+		{
+			close(result->m_fd);
+			result->m_fd = -1;
+			result->m_memory = nullptr;
+			return false;
+		}
+			
+		result->m_length = statInfo.st_size;
+#endif
+
+		result->m_position = 0;
+
+		return true;
 	}
 
 	bool MappedFileStream::IsOpen()
