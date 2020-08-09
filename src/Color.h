a74563e69d2eb24e43daad4c702e21276f0723d1
bellatter <bellatter@gmail.com>
2020-08-09 10:42:20 -0500

    fixed bug in color macro

--- src/Color.h
+++ src/Color.h
@@ -64,7 +64,7 @@ namespace Nxna
 #define NXNA_GET_PACKED_COLOR(c) (Nxna::PackedColor)((unsigned int)c.A << 24 | (unsigned int)c.B << 16 | (unsigned int)c.G << 8 | c.R)
 #define NXNA_GET_PACKED_COLOR_RGB_BYTES(r,g,b) (Nxna::PackedColor)((unsigned int)255 << 24 | (unsigned int)(b & 0xff) << 16 | (unsigned int)(g & 0xff) << 8 | (r & 0xff))
 #define NXNA_GET_PACKED_COLOR_RGBA_BYTES(r,g,b,a) (Nxna::PackedColor)((unsigned int)a << 24 | (unsigned int)(b & 0xff) << 16 | (unsigned int)(g & 0xff) << 8 | (r & 0xff))
-#define NXNA_GET_UNPACKED_COLOR(c, r, g, b, a) { r == c & 0xff; g = (c >> 8) & 0xff; b = (c >> 16) & 0xff; a = (c >> 24) & 0xff; }
+#define NXNA_GET_UNPACKED_COLOR(c, r, g, b, a) { r = (c) & 0xff; g = ((c) >> 8) & 0xff; b = ((c) >> 16) & 0xff; a = ((c) >> 24) & 0xff; }
 
 #ifndef NXNA_COLOR_API_V2
 #define NXNA_COLOR_BLACK          (Nxna::Color {   0,   0,   0, 255 })
